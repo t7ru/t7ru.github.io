04a8bcb29c23245552c3774ed2358f1ec341328a
tofu <t7ru@users.noreply.github.com>
2025-01-08 10:50:54 -0500

    mobile hover

--- oexternal.js
+++ oexternal.js
@@ -1,6 +1,10 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('a')
-    links.forEach(link => {
-      link.setAttribute('target', '_blank')
-    })
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('a')
+  links.forEach(link => {
+    link.setAttribute('target', '_blank')
   })
+})
+
+function toggleHover(element) {
+  element.classList.toggle("active");
+}
