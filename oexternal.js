a2e167f8713b548ad0f8c82f86234949f9a8c115
tofu <t7ru@users.noreply.github.com>
2025-01-08 11:03:28 -0500

    Update oexternal.js

--- oexternal.js
+++ oexternal.js
@@ -1,25 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const links = document.querySelectorAll('a')
+  const links = document.querySelectorAll('a');
   links.forEach(link => {
-    link.setAttribute('target', '_blank')
-  })
-})
+    link.setAttribute('target', '_blank');
+  });
+});
 
 function toggleHover(element) {
   element.classList.toggle("active");
 }
 
-const hoverElements = document.querySelectorAll(".hovercon");
-hoverElements.forEach(el => {
-  el.addEventListener("click", function (event) {
-    event.stopPropagation(); 
-  });
-});
-
 document.addEventListener("click", function (event) {
+  const hoverElements = document.querySelectorAll(".hovercon");
+  let clickedInside = false;
+
   hoverElements.forEach(el => {
-    if (!el.contains(event.target)) {
+    if (el.contains(event.target)) {
+      clickedInside = true;
+    } else {
       el.classList.remove("active");
     }
   });
-});
