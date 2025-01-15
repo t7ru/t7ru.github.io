2a1e1c3ee93b574f359d01951ca8628b6e63e03f
tofu <57356716+t7ru@users.noreply.github.com>
2025-01-14 19:59:51 -0500

    Update lastupdated.js

--- lastupdated.js
+++ lastupdated.js
@@ -1,19 +1,21 @@
 window.onload = function () {
   const repo = "t7ru/t7ru.github.io";
-  const apiUrl = `https://api.github.com/repos/${repo}/commits/main`; // github api url to fetch commit data
+  const apiUrl = `https://api.github.com/repos/${repo}/commits/main`;
 
   fetch(apiUrl)
-    .then((response) => response.json()) // parse the response as json
+    .then((response) => response.json())
     .then((data) => {
-      const lastUpdated = new Date(data.commit.committer.date); // get the commit date
-      const formattedDate = lastUpdated.toLocaleDateString("en-UK", { // format the date
+      const lastUpdated = new Date(data.commit.committer.date);
+      const formattedDate = lastUpdated.toLocaleDateString("en-UK", {
         year: "numeric",
         month: "long",
         day: "numeric",
         hour: "2-digit",
         minute: "2-digit",
       });
-      document.getElementById("lastupdated").innerText = `Last updated on ${formattedDate} (local time.)`; // display formatted date
+      document.getElementById(
+        "lastupdated"
+      ).innerText = `Last updated on ${formattedDate} (local time.)`;
     })
-    .catch((error) => console.error("Error fetching commit data:", error)); // print errors
+    .catch((error) => console.error("Error fetching commit data:", error));
 };
