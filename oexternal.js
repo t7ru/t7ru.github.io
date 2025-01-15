e1abaf4bfbe9def591d7665d418920d71400d499
Benjamin <57356716+t7ru@users.noreply.github.com>
2025-01-14 21:46:09 -0500

    Update oexternal.js

--- oexternal.js
+++ oexternal.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // selects all anchor (<a>) elements on the page
   const links = document.querySelectorAll("a");
   links.forEach((link) => {
-    // checks if the link does NOT have the class 'noex'
+    // checks if the link has the class 'noex' or not, if yes then ignore
     if (!link.classList.contains("noex")) {
       // if the 'noex' class is not present, it sets the 'target' attribute to '_blank'
       link.setAttribute("target", "_blank");
