fc4327492edea662238bd983428bf8a71dea0580
Benjamin <gabonnie1234@gmail.com>
2025-06-09 23:49:26 -0400

    chore: update tabber.js

--- tabber.js
+++ tabber.js
@@ -1,15 +1,14 @@
 function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  const tablinks = document.getElementsByClassName("tablinks");
+  let i;
 
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
@@ -17,4 +16,4 @@ function openCity(evt, cityName) {
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
-} 
+}
